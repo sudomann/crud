017d062716b881a406c0598a6f85c42df69cea5b
Willy Njundong <njundongw@gmail.com>
2019-03-29 18:15:55 -0400

    Hardcoded data tested; postgres serializer added

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import 'dotenv/config';
 import express from 'express';
-
+import cors from 'cors'
 
 
 // .env file usage sample
@@ -8,10 +8,49 @@ import express from 'express';
 
 const app = express();
 
-app.get('/', (req, res) => {
-  res.send('Hello World!');
+app.use(cors());
+
+let courses = {
+  1: {
+    id: '1',
+    courseName: 'Fundamentals of Computer Networking',
+  },
+  2: {
+    id: '2',
+    courseName: 'Software Engineering',
+  },
+};
+
+let notes = {
+  1: {
+    id: '1',
+    text: 'Hello World',
+    courseId: '1',
+  },
+  2: {
+    id: '2',
+    text: 'Hello World Again',
+    courseId: '2',
+  },
+};
+
+app.get('/courses', (req, res) => {
+  return res.send(Object.values(courses));
 });
 
-app.listen(3000, () =>
-  console.log('ExpressJS running at port 3000'),
+app.get('/courses/:courseId', (req, res) => {
+  return res.send(courses[req.params.courseId]);
+});
+
+app.get('/notes', (req, res) => {
+  return res.send(Object.values(notes));
+});
+
+app.get('/notes/:noteId', (req, res) => {
+  return res.send(notes[req.params.noteId]);
+});
+
+
+app.listen(process.env.PORT, () =>
+  console.log(`ExpressJS running at port ${process.env.PORT}`),
 );
