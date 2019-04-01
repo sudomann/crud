623e27c5c6f9dd2402a7e7cd947a11e1284fd26d
Willy Njundong <njundongw@gmail.com>
2019-04-01 14:53:04 -0400

    db seeded
    Ran:  node_modules/.bin/sequelize db:seed:all

--- seeders/20190401184511-seed-note.js
+++ seeders/20190401184511-seed-note.js
@@ -1,17 +1,39 @@
 'use strict';
 
-module.exports = {
+module.exports = {  
   up: (queryInterface, Sequelize) => {
     /*
       Add altering commands here.
       Return a promise to correctly handle asynchronicity.
 
       Example:
-      return queryInterface.bulkInsert('People', [{
+      return queryInterface.bulkInsert('Person', [{
         name: 'John Doe',
         isBetaMember: false
       }], {});
     */
+   return queryInterface.bulkInsert('Notes', [{
+      author: 'Willy Njundong',
+      date: '4/1/2019',
+      text: 'Lorem Ipsum Dolor notes la di dah',
+      email: 'will@gmail.com',
+      createdAt: new Date().toDateString(),
+      updatedAt: new Date().toDateString()
+    }, {
+      author: 'Zach Holochwost',
+      date: 'Doo',
+      text: '444-555-6666',
+      email: 'scooby.doo@misterymachine.com',
+      createdAt: new Date().toDateString(),
+      updatedAt: new Date().toDateString()
+    }, {
+      author: 'Herbie',
+      date: '4/1/2019',
+      text: 'Lorem Ipsum Dolor notes la di dah',
+      email: 'zach@email.edu',
+      createdAt: new Date().toDateString(),
+      updatedAt: new Date().toDateString()
+    }], {});
   },
 
   down: (queryInterface, Sequelize) => {
@@ -20,7 +42,8 @@ module.exports = {
       Return a promise to correctly handle asynchronicity.
 
       Example:
-      return queryInterface.bulkDelete('People', null, {});
+      return queryInterface.bulkDelete('Person', null, {});
     */
+   return queryInterface.bulkDelete('Notes', null, {});
   }
 };
